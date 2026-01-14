6a22cb0c7621db4610a777f411e9a39c1a1c2cd9
Ihor Pavliuk <akira.pavl@gmail.com>
2026-01-14 18:23:29 +0200

    added integration tests for listing service

---
+++ test/integration/listing.service.int-spec.ts
@@ -0,0 +1,35 @@
+import { CreateListingDto } from 'src/modules/listing/dto/create-listing.dto';
+import { ListingService } from 'src/modules/listing/listing.service';
+import { DatabaseService } from 'src/services/database/database.service';
+import { app } from 'test/setup';
+
+describe('ListingService', () => {
+  let listingService: ListingService;
+  let db: DatabaseService;
+
+  beforeAll(() => {
+    listingService = app.get(ListingService);
+    db = app.get(DatabaseService);
+  });
+
+  describe('create()', () => {
+    it('creates new listing', async () => {
+      const newListing: CreateListingDto = {
+        label: 'label',
+        addressCity: 'city',
+        addressLine1: 'address',
+        addressLine2: null,
+        addressProvince: 'province',
+        bathrooms: 1,
+        bedrooms: 1,
+        price: 100,
+        squareMeters: 100,
+      };
+      const result = await listingService.create(newListing);
+      const dbListing = await db.listing.findUnique({
+        where: { id: result.id },
+      });
+      expect(result).toStrictEqual(dbListing);
+    });
+  });
+});
